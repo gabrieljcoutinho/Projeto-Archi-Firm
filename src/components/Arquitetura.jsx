f3aa91293aae23c16acb5735f582402ef9a33942
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-02 11:44:52 -0300

    conteudo

--- src/components/Arquitetura.jsx
+++ src/components/Arquitetura.jsx
@@ -6,7 +6,12 @@ const Arquitetura = (props: Props) => {
   return (
     <div>
 
-            <h2>Our range of architectural services</h2>
+     <div className="tituloTexto">
+     <h2>Our range of architectural services</h2>
+
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est id explicabo dicta eaque expedita perferendis exercitationem dolorum molestiae similique, eveniet, suscipit quos officia voluptatibus voluptatum neque aliquam quo libero doloremque?</p>
+     </div>
+
 
     </div>
   )
