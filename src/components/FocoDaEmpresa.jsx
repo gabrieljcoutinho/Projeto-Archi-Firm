971efb6afa2afee8ba92e45e3e10f724fe3f7860
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-02 14:36:24 -0300

    conteudo

--- src/components/FocoDaEmpresa.jsx
+++ src/components/FocoDaEmpresa.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const FocoDaEmpresa = () => {
   return (
-    <div><h4>We consistently focus on both beauty and usability</h4></div>
+    <div>
+
+        <h4>We consistently focus on both beauty and usability</h4>
+
+<div className='secao1'>
+
+</div>
+
+<div className='secao2'>
+
+</div>
+
+
+    </div>
   )
 }
 
