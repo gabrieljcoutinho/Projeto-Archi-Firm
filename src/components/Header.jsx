6e0f14bef03b7b0f8345323178435a927d197e53
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-02 10:48:36 -0300

    conteudo ehraser

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => {
   return (
     <div>
            <ul className="headerMenu">
-            <li className="headerMenuLista"></li>
-            <li className="headerMenuLista"></li>
-            <li className="headerMenuLista"></li>
-            <li className="headerMenuLista"></li>
+            <li className="headerMenuLista"><a href="#" className='linkListaHeader'>About</a></li>
+            <li className="headerMenuLista"><a href="#" className='linkListaHeader'>Service</a></li>
+            <li className="headerMenuLista"><a href="#" className='linkListaHeader'>Blog</a></li>
+            <li className="headerMenuLista"><a href="#" className='linkListaHeader'>Careers</a></li>
            </ul>
 
 
