b3ac61eb57c9577de3510c2cc3c9ee15a55d3770
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-02 15:03:58 -0300

    detsalhe

--- src/components/FaixaSeparandoConteudo2.jsx
+++ src/components/FaixaSeparandoConteudo2.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
-type Props = {}
 
-const FaixaSeparandoConteudo2 = (props: Props) => {
+const FaixaSeparandoConteudo2 = () => {
   return (
     <div>FaixaSeparandoConteudo2</div>
   )
