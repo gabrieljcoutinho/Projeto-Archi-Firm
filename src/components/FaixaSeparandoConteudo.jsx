9627a86816365d236f1110f01fa1eca244129b2b
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-02 14:07:23 -0300

    nova img

--- src/components/FaixaSeparandoConteudo.jsx
+++ src/components/FaixaSeparandoConteudo.jsx
@@ -5,6 +5,7 @@ import logoFaixaLAranja from "../img/logoFaixaLaranja1.png"
 import logoFaixaLAranja2 from "../img/logoFaixaLaranja2.png"
 import logoFaixaLAranja3 from "../img/logoFaixaLaranja3.png"
 import logoFaixaLAranja4 from "../img/logoFaixaLaranja4.png"
+import logoFaixaLAranja5 from "../img/logoFaixaLaranja5.png"
 
 const FaixaSeparandoConteudo = () => {
   return (
