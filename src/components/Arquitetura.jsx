19ba915f486107cfbdfd3d2b0958631c1d102ee8
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-02 12:48:16 -0300

    descricao arquitetura

--- src/components/Arquitetura.jsx
+++ src/components/Arquitetura.jsx
@@ -25,6 +25,11 @@ const Arquitetura = () => {
     <div className="arquiteturasDivConteudo">
 <img src={imgArquitetura1} alt="" />
 <h3>Interior design</h3>
+<p className="descricaoArquitetura">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam odit sit praesentium sequi
+     exercitationem in ab officia doloribus repudiandae culpa magni delectus ratione deserunt
+      incidunt, rerum mollitia temporibus quae. Ea.
+</p>
     </div>
 
     <div className="arquiteturasDivConteudo">
