060d338b98cafc0a6cc77962f111a9298eec8c3a
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-02 10:49:36 -0300

    logo header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "../Css/Header.css"
 
+import logo from "../img/logoHeader.png"
 
 const Header = () => {
   return (
     <div>
            <ul className="headerMenu">
+            <img src={logo} alt="logo" />
             <li className="headerMenuLista"><a href="#" className='linkListaHeader'>About</a></li>
             <li className="headerMenuLista"><a href="#" className='linkListaHeader'>Service</a></li>
             <li className="headerMenuLista"><a href="#" className='linkListaHeader'>Blog</a></li>
