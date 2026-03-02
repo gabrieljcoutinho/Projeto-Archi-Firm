3b4d16b739c40f6f2c8a663480cff2bcb67579f8
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-02 15:03:39 -0300

    novo conteudo

---
+++ src/components/FaixaSeparandoConteudo2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const FaixaSeparandoConteudo2 = (props: Props) => {
+  return (
+    <div>FaixaSeparandoConteudo2</div>
+  )
+}
+
+export default FaixaSeparandoConteudo2
