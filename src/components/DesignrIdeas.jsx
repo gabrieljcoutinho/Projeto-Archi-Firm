d84a20d4e734ea05b58fdefa4d59f7e6715a84be
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-02 16:06:55 -0300

    paragafo

--- src/components/DesignrIdeas.jsx
+++ src/components/DesignrIdeas.jsx
@@ -44,6 +44,7 @@ const DesignrIdeas = () => {
                                     <div className="divs">
                                     <img src={img3} alt="" />
                                     <h6>Modernizing a classic loft with serene interiors</h6>
+                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et dignissimos quis, laudantium assumenda iste est sunt quod tenetur sequi magni corrupti inventore optio eius possimus voluptas architecto dolor qui aspernatur.</p>
                                     </div>
 
                         </div>
