ba3de2e1751d992f36efadb9838d3f6aee1990c4
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-02 15:08:59 -0300

    titul oseção

--- src/components/FaixaSeparandoConteudo2.jsx
+++ src/components/FaixaSeparandoConteudo2.jsx
@@ -4,7 +4,11 @@ import "../../src/Css/faixaLaranjaSeparandoConteudo2.css"
 
 const FaixaSeparandoConteudo2 = () => {
   return (
-    <div className='FaixaSeparandoConteudo2'>FaixaSeparandoConteudo2</div>
+    <div className='FaixaSeparandoConteudo2'>
+
+      <h2>Start your architectural journey with us</h2>
+
+      </div>
   )
 }
 
