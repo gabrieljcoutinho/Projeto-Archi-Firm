fd07ef33b6ff15b8e62f4c26f39055245a28f5c6
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-02 15:34:20 -0300

    conteu o pragra + classe

--- src/components/DesignrIdeas.jsx
+++ src/components/DesignrIdeas.jsx
@@ -8,6 +8,9 @@ const DesignrIdeas = () => {
                         <div className="conteudoInicialDesigner">
                         <h4>Exploring design <br />
                         trends and ideas</h4>
+
+                        <p className="paragrafoDescricaoDesgnerIdeas">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quod, exercitationem molestiae id nam, vel quam et aspernatur blanditiis nostrum. Aliquam vitae distinctio sapiente assumenda pariatur quo! Ducimus, nesciunt.</p>
+
                         </div>
 
     </div>
