03b3d68ffa2894f880d8f1f6c71285526607e2ba
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-02 13:39:45 -0300

    img improt

--- src/components/SectionImgDesign.jsx
+++ src/components/SectionImgDesign.jsx
@@ -11,6 +11,8 @@ const SectionImgDesign = () => {
         <h3 className='tituloSectionDesign'>Step inside our design journey:< br/>
              an exclusive video glimpse</h3>
 
+             <img src={exclusiveVideo} alt="" />
+
     </div>
   )
 }
