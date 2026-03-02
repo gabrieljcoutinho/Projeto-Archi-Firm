555c8db0a7b18257664589118e53dc230332504f
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-02 11:54:39 -0300

    teste

--- src/components/Arquitetura.jsx
+++ src/components/Arquitetura.jsx
@@ -9,7 +9,9 @@ const Arquitetura = () => {
             <h2 className='titulo'>Our range of <br />
                 architectural services</h2>
 
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est id explicabo dicta eaque expedita perferendis exercitationem dolorum molestiae similique, eveniet, suscipit quos officia voluptatibus voluptatum neque aliquam quo libero doloremque?</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est id explicabo dicta eaque <br/>
+                        expedita perferendis exercitationem dolorum molestiae similique, eveniet, suscipit quos<br/>
+                         officia voluptatibus voluptatum neque aliquam quo libero doloremque?</p>
             </div>
 
 
