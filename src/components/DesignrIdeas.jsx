12f3366d507bcbcae3f6c7c7af46b1c5995b13d1
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-02 15:36:26 -0300

    espaçamento

--- src/components/DesignrIdeas.jsx
+++ src/components/DesignrIdeas.jsx
@@ -6,10 +6,14 @@ const DesignrIdeas = () => {
     <div className='designers'>
 
                         <div className="conteudoInicialDesigner">
-                        <h4 className='subTituloDesgner'>Exploring design <br />
-                        trends and ideas</h4>
+                                    <div className="conteudoDesigner1">
+                                    <h4 className='subTituloDesgner'>Exploring design <br />
+                                    trends and ideas</h4>
+                                    </div>
 
-                        <p className="paragrafoDescricaoDesgnerIdeas">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quod, exercitationem molestiae id nam, vel quam et aspernatur blanditiis nostrum. Aliquam vitae distinctio sapiente assumenda pariatur quo! Ducimus, nesciunt.</p>
+                                        <div className="conteudoDesginer2">
+                                        <p className="paragrafoDescricaoDesgnerIdeas">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quod, exercitationem molestiae id nam, vel quam et aspernatur blanditiis nostrum. Aliquam vitae distinctio sapiente assumenda pariatur quo! Ducimus, nesciunt.</p>
+                                        </div>
 
                         </div>
 
