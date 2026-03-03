f70e8414a1bd4a080a7c126e34572525f0ef1456
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-03 11:05:06 -0300

    classe

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
         <ul className="linksRedesSociais">
           <li><a href="#" className='itemListaRedesSociais'><FaFacebookF /></a></li>
           <li><a href="#" className='itemListaRedesSociais'><FaTwitter /></a></li>
-          <li><a href="#"><FaInstagram /></a></li>
+          <li><a href="#" className='itemListaRedesSociais'><FaInstagram /></a></li>
           <li><a href="#"><FaLinkedinIn /></a></li>
           <li><a href="#"><FaYoutube /></a></li>
         </ul>
