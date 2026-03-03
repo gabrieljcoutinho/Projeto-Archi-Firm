408b4a0e6c2656aa56370bb8ed86137b410f5e59
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-03 09:41:05 -0300

    lista +- iagual

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,6 +25,19 @@ const Footer = () => {
                     </ul>
 
 
+                    <ul className="categorias">
+              <h3>Category</h3>
+                  <li><a href="#">Features</a></li>
+                  <li><a href="#">Pricing</a></li>
+                  <li><a href="#">Case studies</a></li>
+                  <li><a href="#">Reviews</a></li>
+                  <li><a href="#">Updates</a></li>
+
+                    </ul>
+
+
+
+
     </div>
   )
 }
